be054bce7e8a44e437b23f92a2d3ac3dff06e469
bird0401 <mhiro7777777@gmail.com>
2024-06-17 17:19:59 +0900

    [update]

--- backend/internal/middleware/router.go
+++ backend/internal/middleware/router.go
@@ -3,6 +3,7 @@ package middleware
 import (
 	"myapp/internal/external"
 	"myapp/internal/repositories"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
@@ -12,6 +13,13 @@ func SetupRoutes(app *gin.Engine) {
 	postRepository := repositories.NewPostRepository(db)
 	userRepository := repositories.NewUserRepository(db)
 	h := NewHandler(postRepository, userRepository)
+
+	app.POST("/signin", h.PostSignin)
+	authGroup := app.Group("/auth")
+	authGroup.Use(AuthMiddleware)
+	authGroup.GET("", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{"message": "you are authorized"})
+	})
 
 	app.GET("/", func(ctx *gin.Context) {
 		ctx.String(200, "It works")
--- backend/main.go
+++ backend/main.go
@@ -5,7 +5,6 @@ import (
 	"myapp/internal/config"
 	"myapp/internal/external"
 	"myapp/internal/middleware"
-	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
@@ -19,12 +18,6 @@ func main() {
 	app.Use(middleware.Transaction())
 	app.Use(middleware.Cors())
 	middleware.SetupRoutes(app)
-
-	authGroup := app.Group("/auth")
-	authGroup.Use(middleware.AuthMiddleware)
-	authGroup.GET("", func(ctx *gin.Context) {
-		ctx.JSON(http.StatusOK, gin.H{"message": "you are authorized"})
-	})
 
 	app.Run(fmt.Sprintf("%s:%d", config.HostName, config.Port))
 }
