e90bfb63f8cda515ae64318894bb8f1f44ac0446
Kohei Okazaki <kohei.a.okazaki@dena.com>
2024-06-17 15:59:24 +0900

    feat: 投稿詳細画面に、編集・削除・新規投稿の機能追加

--- frontend/src/shared/services/mockApiService.ts
+++ frontend/src/shared/services/mockApiService.ts
@@ -1,15 +1,40 @@
-import { Post } from '../models/Post';
+import {Post} from '../models/Post';
+
+// モックデータ
+let posts: Post[] = [
+    {
+        id: 1,
+        title: 'バナナは持っていっていいですか？',
+        username: 'testuser',
+        createdAt: '2022/4/25 18:38:00',
+        updatedAt: '2022/4/25 18:38:00',
+        content: '２本目も持って行っていいですかね？'
+    },
+    {
+        id: 2,
+        title: 'test2',
+        username: 'hoge',
+        createdAt: '2022/4/25 18:38:00',
+        updatedAt: '2022/4/25 18:38:00',
+        content: 'これは二つ目のテストです。'
+    },
+];
 
 export const fetchPosts = async (): Promise<Post[]> => {
-    // モックデータ
-    const mockPosts: Post[] = [
-        { id: 1, title: 'バナナは持っていっていいですか？', username: 'hoge', createdAt: '2022/4/25 18:38:00', updatedAt: '2022/4/25 18:38:00', content: '２本目も持って行っていいですかね？' },
-        { id: 2, title: 'test2', username: 'hoge', createdAt: '2022/4/25 18:38:00', updatedAt: '2022/4/25 18:38:00', content: 'これは二つ目のテストです。' },
-    ];
-
     return new Promise(resolve => {
         setTimeout(() => {
-            resolve(mockPosts);
+            resolve(posts);
         }, 1000); // 1秒の遅延をシミュレート
     });
+};
+
+export const deletePost = async (postId: number): Promise<void> => {
+    posts = posts.filter(post => post.id !== postId);
+}
+
+export const updatePost = async (postId: number, data: { title: string, content: string }): Promise<void> => {
+    const postIndex = posts.findIndex(post => post.id === postId);
+    if (postIndex > -1) {
+        posts[postIndex] = {...posts[postIndex], ...data, updatedAt: new Date().toISOString()};
+    }
 };
