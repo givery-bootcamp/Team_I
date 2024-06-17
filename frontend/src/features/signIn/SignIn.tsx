5716bcaf3e432c82423a4ff946bf81ec28c5de5a
chihirokubo <nowarch1204@yahoo.co.jp>
2024-06-17 14:29:55 +0900

    サインインエラー後の処理の修正

--- frontend/src/features/signIn/SignIn.tsx
+++ frontend/src/features/signIn/SignIn.tsx
@@ -35,6 +35,7 @@ const SignIn: React.FC = () => {
             if (!response.ok) {
                 // 失敗したらサインインエラー
                 setSignInError(await response.json())
+                return;
             }
             
             // jwtを保存
