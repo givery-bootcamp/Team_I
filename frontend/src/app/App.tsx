8588c36ab045d5f7316f5898a331f05d1987083c
Kohei Okazaki <kohei.a.okazaki@dena.com>
2024-06-17 11:55:20 +0900

    feat: ヘッダーにサインイン状態を表示する機能を追加
    - SignInStatusコンポーネントを作成し、サインイン状態を表示するように変更
    - Headerコンポーネントを更新し、サインインボタンとサインアウトボタンを適切に表示
    - SignInコンポーネントにテストユーザーでのサインイン検証ロジックを追加
    - ユーザーがサインインしている場合はサインインボタンを非表示にし、サインアウトボタンを表示
    - サインインしていない場合はサインインボタンを表示

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,19 +1,26 @@
-import { BrowserRouter as Router } from 'react-router-dom';
+import {BrowserRouter as Router} from 'react-router-dom';
 import Navigation from '../shared/components/Navigation';
-import AppRouter from './AppRouter.tsx';
+import Header from '../shared/components/Header';
+import AppRouter from './AppRouter';
+import {AuthProvider} from '../shared/components/AuthContext';
 
 function App() {
     return (
-        <Router>
-            <div className="flex">
-                <aside className="w-1/4">
-                    <Navigation />
-                </aside>
-                <main className="w-3/4 p-4">
-                    <AppRouter />
-                </main>
-            </div>
-        </Router>
+        <AuthProvider>
+            <Router>
+                <div className="flex flex-col min-h-screen">
+                    <Header/>
+                    <div className="flex flex-1">
+                        <aside className="w-1/4">
+                            <Navigation/>
+                        </aside>
+                        <main className="w-3/4 p-4">
+                            <AppRouter/>
+                        </main>
+                    </div>
+                </div>
+            </Router>
+        </AuthProvider>
     );
 }
 
