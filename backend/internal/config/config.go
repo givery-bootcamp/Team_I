9f05a854ef8f50a16250c7e7d194bb0f75c15d22
Kohei Okazaki <kohei.a.okazaki@dena.com>
2024-06-18 15:28:18 +0900

    squash! fix(backend): データベース接続情報を更新

--- backend/internal/config/config.go
+++ backend/internal/config/config.go
@@ -8,7 +8,7 @@ import (
 var HostName = "127.0.0.1"
 var Port = 9000
 var CorsAllowOrigin = "http://localhost:3000"
-var DBHostName = "172.19.0.3"
+var DBHostName = "db"
 var DBPort = 3306
 var DBName = "training"
 var DBUsername = "user"
