7430a3a25018765ddb092c8457c6e434339a61c2
chocono2230 <61158776+chocono2230@users.noreply.github.com>
2024-06-28 20:10:25 +0900

    attend状態の管理について修正

--- frontend/src/features/postDetail/PostDetail.tsx
+++ frontend/src/features/postDetail/PostDetail.tsx
@@ -147,7 +147,6 @@ const PostDetail: React.FC = () => {
 
     const {showAlert} = useAlert();
 
-    const [intention, setIntention] = useState<IntentionState | null>(null);
     const [attendees, setAttendees] = useState<string[]>([]);
     const [nonAttendees, setNonAttendees] = useState<string[]>([]);
 
@@ -185,14 +184,23 @@ const PostDetail: React.FC = () => {
         // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
         fetchIntentionState(parseInt(postId!, 10), 'attend')
             .then(data => {
-                if(!!data.usernames) setAttendees(data.usernames)
+                console.log(data);
+                if(!!data) {
+                    const dataArray = data as any[];
+                    const arr = dataArray.map((item) => item.name);
+                    setAttendees(arr)
+                }
             }); // Adjust as per your API response
 
         // Load non-attendees
         // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
         fetchIntentionState(parseInt(postId!, 10), 'skip')
             .then(data => {
-                if(!!data.usernames) setNonAttendees(data.usernames)
+                if(!!data) {
+                    const dataArray = data as any[];
+                    const arr = dataArray.map((item) => item.name);
+                    setNonAttendees(arr)
+                }
             }); // Adjust as per your API response
     }, [postId]);
 
@@ -202,12 +210,22 @@ const PostDetail: React.FC = () => {
             return;
         }
 
+        const addOrErase = (arr: string[], setArr: React.Dispatch<React.SetStateAction<string[]>>, other: string[],setOther: React.Dispatch<React.SetStateAction<string[]>>, name: string) => {
+            if (other.includes(name)) {
+                setOther(other.filter(item => item !== name));
+            }
+            if (arr.includes(name)) {
+                setArr(arr.filter(item => item !== name));
+            } else {
+                setArr([...arr, name]);
+            }
+        }
+
         postIntention(post.id, intention, userId)
             .then(() => {
-                setIntention(intention);
                 intention === 'attend' ?
-                    setAttendees(prev => [...prev, user?.name]) :
-                    setNonAttendees(prev => [...prev, user?.name]);
+                    addOrErase(attendees, setAttendees, nonAttendees, setNonAttendees, user?.name):
+                    addOrErase(nonAttendees, setNonAttendees, attendees, setAttendees, user?.name);
             })
             .catch((error) => {
                 console.error(error);
@@ -315,7 +333,7 @@ const PostDetail: React.FC = () => {
                                 onClick={() => handleIntention('attend')}
                                 disabled={loading}
                                 className={`flex-1 mr-2 py-2 px-4 rounded bg-blue-500 hover:bg-blue-600 text-white font-semibold
-                            ${intention === 'attend' ? 'opacity-100' : 'opacity-50'}`}
+                            ${user && attendees.includes(user?.name) ? 'opacity-100' : 'opacity-50'}`}
                             >
                                 参加
                             </button>
@@ -323,7 +341,7 @@ const PostDetail: React.FC = () => {
                                 onClick={() => handleIntention('skip')}
                                 disabled={loading}
                                 className={`flex-1 ml-2 py-2 px-4 rounded bg-red-500 hover:bg-red-600 text-white font-semibold
-                            ${intention === 'skip' ? 'opacity-100' : 'opacity-50'}`}
+                            ${user && nonAttendees.includes(user?.name) ? 'opacity-100' : 'opacity-50'}`}
                             >
                                 不参加
                             </button>
