e85c7992286aee109c609f991264a405d5a2e0b8
Kohei Okazaki <kohei.a.okazaki@dena.com>
2024-06-28 17:46:18 +0900

    feat(database): データベースシード機能を追加
    データベースに初期データを挿入するためのシード機能を実装しました。これにより、ユーザーや投稿、コメントのデータをプログラムから手動で挿入することなく自動でデータを生成できます。また、各SQLファイルからダイレクトにデータを挿入するロジックを削除しました。

--- mysql/sql/insert.sql
+++ mysql/sql/insert.sql
@@ -1,45 +1,25 @@
-INSERT INTO users (name, password) VALUES ('taro', '$2a$10$P9Zr9LES1Yv/n6k77pDy0OVwCRBeHRhHsFMQyU6GfkfpOXfHOPjgG');
-INSERT INTO users (name, password) VALUES ('hanako', '$2a$10$fyv7Ngey56irY9RkEG0MQuDzUyklKkWuqVV86gR3yTMxD6WeNZtZC');
+func
+seedDatabase(db *gorm.DB) {
+	users := []database.User{
+		{Name: "taro", Password: "$2a$10$P9Zr9LES1Yv/n6k77pDy0OVwCRBeHRhHsFMQyU6GfkfpOXfHOPjgG"},
+		{Name: "hanako", Password: "$2a$10$fyv7Ngey56irY9RkEG0MQuDzUyklKkWuqVV86gR3yTMxD6WeNZtZC"},
+	}
 
-INSERT INTO posts (user_id, title, body) VALUES (1, 'test1', '質問1\n改行');
-INSERT INTO posts (user_id, title, body)
-VALUES (2, 'test2', '質問2\n改行');
-INSERT INTO posts (user_id, title, body)
-VALUES (1, 'test3', '質問1\n改行');
-INSERT INTO posts (user_id, title, body)
-VALUES (1, 'test4', '質問2\n改行');
-INSERT INTO posts (user_id, title, body)
-VALUES (2, 'test5', '質問1\n改行');
-INSERT INTO posts (user_id, title, body)
-VALUES (1, 'test6', '質問2\n改行');
-INSERT INTO posts (user_id, title, body)
-VALUES (1, 'test7', '質問1\n改行');
-INSERT INTO posts (user_id, title, body)
-VALUES (2, 'test8', '質問2\n改行');
-INSERT INTO posts (user_id, title, body)
-VALUES (1, 'test9', '質問1\n改行');
-INSERT INTO posts (user_id, title, body)
-VALUES (1, 'test10', '質問2\n改行');
-INSERT INTO posts (user_id, title, body)
-VALUES (1, 'atest1', '質問1\n改行');
-INSERT INTO posts (user_id, title, body)
-VALUES (2, 'atest2', '質問2\n改行');
-INSERT INTO posts (user_id, title, body)
-VALUES (1, 'atest3', '質問1\n改行');
-INSERT INTO posts (user_id, title, body)
-VALUES (1, 'atest4', '質問2\n改行');
-INSERT INTO posts (user_id, title, body)
-VALUES (2, 'atest5', '質問1\n改行');
-INSERT INTO posts (user_id, title, body)
-VALUES (1, 'atest6', '質問2\n改行');
-INSERT INTO posts (user_id, title, body)
-VALUES (1, 'atest7', '質問1\n改行');
-INSERT INTO posts (user_id, title, body)
-VALUES (2, 'atest8', '質問2\n改行');
-INSERT INTO posts (user_id, title, body)
-VALUES (1, 'atest9', '質問1\n改行');
-INSERT INTO posts (user_id, title, body)
-VALUES (1, 'atest10', '質問2\n改行');
+	for i, user := range users {
+		result := db.Create(&user)
+		if result.Error != nil {
+			log.Fatal("failed to seed users: ", result.Error)
+		}
 
-INSERT INTO comments (user_id, post_id, body) VALUES (2, 1, '初コメ\n改行');
-INSERT INTO comments (user_id, post_id, body) VALUES (2, 1, '2コメ\n改行');
+		posts := []database.Post{
+			{UserID: user.ID, Title: "test1", Body: "質問1\n改行"},
+			{UserID: user.ID, Title: "test2", Body: "質問2\n改行"},
+			{UserID: user.ID, Title: "test3", Body: "質問1\n改行"},
+		}
+
+		if i == 0 {
+			posts = append(posts, []database.Post{
+				{UserID: user.ID, Title: "test4", Body: "質問2\n改行"},
+				{UserID: user.ID, Title: "test7", Body: "質問1\n改行"},
+				{UserID: user.ID, Title: "test9", Body: "質問1\n改行"},
+				{UserID:
