e066b33752b0b447bfa45e806dd3dc0953320f63
chocono2230 <61158776+chocono2230@users.noreply.github.com>
2024-06-28 16:00:00 +0900

    service層追加

---
+++ backend/internal/entities/services/service_helper.go
@@ -0,0 +1,16 @@
+package services
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+type ErrorResponse struct {
+	Message string `json:"message"`
+}
+
+func handleError(ctx *gin.Context, status int, err error) {
+	res := ErrorResponse{
+		Message: err.Error(),
+	}
+	ctx.JSON(status, &res)
+}
